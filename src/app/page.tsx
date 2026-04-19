3117974e30f413396e2b017e03f967f053123203
Bustrama <rubik.junk@gmail.com>
2026-04-20 02:00:31 +0300

    feat: use official Claude and Bambu Lab logos in header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,10 +103,16 @@ export default function Home() {
 function ClaudeMark() {
   return (
     <span style={styles.mark} aria-label="Claude" title="Claude">
-      <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        width="22"
+        height="22"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ color: "#D97757" }}
+      >
         <path
-          d="M12 2 L13.6 9.2 C13.85 10.3 14.7 11.15 15.8 11.4 L23 13 L15.8 14.6 C14.7 14.85 13.85 15.7 13.6 16.8 L12 24 L10.4 16.8 C10.15 15.7 9.3 14.85 8.2 14.6 L1 13 L8.2 11.4 C9.3 11.15 10.15 10.3 10.4 9.2 Z"
-          fill="#D97757"
+          fill="currentColor"
+          d="m19.6 66.5 19.7-11 .3-1-.3-.5h-1l-3.3-.2-11.2-.3L14 53l-9.5-.5-2.4-.5L0 49l.2-1.5 2-1.3 2.9.2 6.3.5 9.5.6 6.9.4L38 49.1h1.6l.2-.7-.5-.4-.4-.4L29 41l-10.6-7-5.6-4.1-3-2-1.5-2-.6-4.2 2.7-3 3.7.3.9.2 3.7 2.9 8 6.1L37 36l1.5 1.2.6-.4.1-.3-.7-1.1L33 25l-6-10.4-2.7-4.3-.7-2.6c-.3-1-.4-2-.4-3l3-4.2L28 0l4.2.6L33.8 2l2.6 6 4.1 9.3L47 29.9l2 3.8 1 3.4.3 1h.7v-.5l.5-7.2 1-8.7 1-11.2.3-3.2 1.6-3.8 3-2L61 2.6l2 2.9-.3 1.8-1.1 7.7L59 27.1l-1.5 8.2h.9l1-1.1 4.1-5.4 6.9-8.6 3-3.5L77 13l2.3-1.8h4.3l3.1 4.7-1.4 4.9-4.4 5.6-3.7 4.7-5.3 7.1-3.2 5.7.3.4h.7l12-2.6 6.4-1.1 7.6-1.3 3.5 1.6.4 1.6-1.4 3.4-8.2 2-9.6 2-14.3 3.3-.2.1.2.3 6.4.6 2.8.2h6.8l12.6 1 3.3 2 1.9 2.7-.3 2-5.1 2.6-6.8-1.6-16-3.8-5.4-1.3h-.8v.4l4.6 4.5 8.3 7.5L89 80.1l.5 2.4-1.3 2-1.4-.2-9.2-7-3.6-3-8-6.8h-.5v.7l1.8 2.7 9.8 14.7.5 4.5-.7 1.4-2.6 1-2.7-.6-5.8-8-6-9-4.7-8.2-.5.4-2.9 30.2-1.3 1.5-3 1.2-2.5-2-1.4-3 1.4-6.2 1.6-8 1.3-6.4 1.2-7.9.7-2.6v-.2H49L43 72l-9 12.3-7.2 7.6-1.7.7-3-1.5.3-2.8L24 86l10-12.8 6-7.9 4-4.6-.1-.5h-.3L17.2 77.4l-4.7.6-2-2 .2-3 1-1 8-5.5Z"
         />
       </svg>
     </span>
@@ -115,20 +121,15 @@ function ClaudeMark() {
 
 function BambuMark() {
   return (
-    <span style={{ ...styles.mark, background: "#00ae42" }} aria-label="Bambu Lab" title="Bambu Lab">
-      <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M20 3 C12 4 6 9 4 17 C3.5 19 3.5 20.5 4 21 C4.5 21.5 6 21.5 8 21 C16 19 20.5 13.5 21 5 C21 4 20.8 3.3 20.5 3.1 C20.3 3 20.1 3 20 3 Z"
-          fill="#ffffff"
-        />
-        <path
-          d="M5 20 C9 14 14 10 20 7"
-          stroke="#00ae42"
-          strokeWidth="1.2"
-          strokeLinecap="round"
-          fill="none"
-        />
-      </svg>
+    <span style={styles.mark} aria-label="Bambu Lab" title="Bambu Lab">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="https://store.bblcdn.com/s2/default/febc4874843645f38149a05caa0f8a6d/logo.png"
+        alt="Bambu Lab"
+        width={24}
+        height={24}
+        style={{ objectFit: "contain" }}
+      />
     </span>
   );
 }
